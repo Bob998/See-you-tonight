fc261b2ac475c76e65818f65f9c711495bd930a6
Bob998 <38448790+Bob998@users.noreply.github.com>
2020-01-17 11:16:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -32,11 +32,7 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        //bannar_coming: "颜色",
-        //balloons_flying: "好像少点东西",
-        //cake_fadein: "蛋糕？",
-        //light_candle: "蜡烛？",
-        //wish_message: "生日快乐",
+        balloons_flying: "好像少点东西",
         story: "A MESSAGE FOR YOU",
     }
 };
